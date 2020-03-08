fe4e380239c5a4e49b48c63c12bbcd6a4521518d
Jesus Seijas <jesus.seijas@axa.com>
2020-03-08 21:00:08 +0100

    fix: minor linter fixes

--- example/index.js
+++ example/index.js
@@ -4,10 +4,17 @@ function onHear(connector, input) {
   if (input.message.toLowerCase() === 'quit') {
     connector.destroy();
   } else {
-    connector.say(input.message.split('').reverse().join(''));
+    connector.say(
+      input.message
+        .split('')
+        .reverse()
+        .join('')
+    );
   }
 }
 
 const connector = new ConsoleConnector({ onHear });
 connector.initialize();
-connector.say('Say something, I will reverse it! Say "quit" to end the conversation');
+connector.say(
+  'Say something, I will reverse it! Say "quit" to end the conversation'
+);
